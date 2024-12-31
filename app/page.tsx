2f44b939ef2fb511e4ed0f860f9ab45460777d26
akhil683 <onkar.palsra@gmail.com>
2024-12-31 13:39:39 +0530

    feat: github auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import { auth } from "@/auth";
+import SignIn from "@/components/sign-in";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
   return (
     <section>
-      Hello
+      <SignIn />
+      {session?.user?.name}
+      {session?.user?.email}
+      <Image
+        src={session?.user?.image!}
+        width={200}
+        height={200}
+        alt="Helllo"
+      />
     </section>
   );
 
