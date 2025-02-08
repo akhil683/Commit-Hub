b271fe1799ee2f4e39578e254b8e4595626968d9
akhil683 <onkar.palsra@gmail.com>
2025-02-08 22:57:59 +0530

    fix: user-profile undefined

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -15,16 +15,12 @@ export const SignIn = () => {
     setLoading(true)
     try {
       await signIn("github", { callbackUrl: '/(protected)/user-profile' })
-      toast({
-        title: "Login Success !",
-        description: "You are logged in successfully"
-      })
     } catch (SignInError) {
       console.log("Error during signin: ", SignInError)
-      // toast({
-      //   title: "Error !",
-      //   description: "Failed to signin."
-      // })
+      toast({
+        title: "Error !",
+        description: "Failed to signin."
+      })
     } finally {
       setLoading(false)
     }
